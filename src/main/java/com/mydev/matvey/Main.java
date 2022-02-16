46477e0de2194f1b3a26970453033c36e17015af
Sl1ed <antaev2010@gmail.com>
2022-02-16 21:12:04 +0300

    Added simple exceptions handling

--- src/main/java/com/mydev/matvey/Main.java
+++ src/main/java/com/mydev/matvey/Main.java
@@ -1,25 +1,35 @@
 package com.mydev.matvey;
 
-import java.io.IOException;
+
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Enter url");
         Scanner input = new Scanner(System.in);
         String url = input.nextLine();
         System.out.println("Enter the path where to save the html page");
-        Path path = Path.of(input.nextLine());
+        Path path = Path.of(input.nextLine(), "result.html");
         input.close();
-        DownloadPage downloadPage = new DownloadPage();
-        Map<String, Integer> wordsMap = WordCounterUtil.calcFrequency(downloadPage.connectPage(url, path));
 
-        for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
-            System.out.println(entry.getKey() + " - " + entry.getValue());
+        try {
+            DownloadPage downloadPage = new DownloadPage();
+            downloadPage.connectPage(url, path);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+        try {
+            Map<String, Integer> wordsMap = WordCounterUtil.calcFrequency(path);
+            System.out.println("Word frequency statistics:");
+            for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
